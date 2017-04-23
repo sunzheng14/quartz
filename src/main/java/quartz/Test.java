87b7378e93579d90bb3bd7d5e6eeca1231bbfc38
sunzheng <sunzheng@.lvmama.com>
2017-04-23 14:36:16 +0800

    tijiao

--- src/main/java/quartz/Test.java
+++ src/main/java/quartz/Test.java
@@ -3,5 +3,17 @@ package quartz;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello");
+		Test t = new Test();
+		t.test();
+	}
+	
+	public void test(){
+		new Thread(new Runnable() {
+		public void run() {
+			for(int i =0;i<10;i++){
+				System.out.println(Thread.currentThread().getName() + "," +i);
+				}
+			}
+		}).start();
 	}
 }
